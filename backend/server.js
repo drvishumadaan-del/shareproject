c23445bc092f5b21cde91f14c746e2b80abbda53
drvishumadaan-del <dr.vishumadaan@gmail.com>
2026-05-21 12:30:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app=express();
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://drvishumadaan_db_user:12345@cluster0.wpazmhu.mongodb.net/")
-const feeduser=mongoose.model("feeduser",{feed:String})
+mongoose.connect(
+"mongodb+srv://drvishumadaan_db_user:12345@cluster0.wpazmhu.mongodb.net/testdb"
+);
+
+const feeduser = mongoose.model("feeduser", {
+   feed: String
+});
 
 app.post("/feedusers", async (req, res) => {
-  const feeduser = new feeduser({ feed: req.body.feed });
-  await feeduser.save();
-  res.send(feeduser);
+
+   const newFeedUser = new feeduser({
+      feed: req.body.feed
+   });
+
+   await newFeedUser.save();
+
+   res.send(newFeedUser);
 });
 
 app.listen(3000, () => {
-  console.log("Server running on 3000");
+   console.log("Server running on 3000");
 });
-
